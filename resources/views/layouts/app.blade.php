02b69d92a1c088cb0f781c3a1110949ec02d0ef8
unknown <ryan.kadel88@gmail.com>
2020-08-31 17:06:16 -0400

    layout commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'LARA-APP')}}</title>
+    </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="contatiner">
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home</h1>
+    <div class="jumbotron text-center">
+        <h1>My First Laravel Project</h1>
+        <p>This is the project from Traversy Media's "Laravel From Scratch" YouTube series.</p>
+        <p>
+            <a class="btn btn-primary btn-lg" 
+            href="/login" 
+            role="button">
+            Login
+        </a>
+            <a class="btn btn-success btn-lg" 
+            href="/register" 
+            role="button">
+            Register
+            </a>
+        </p>
+    </div>
 @endsection
